6cc753e13d81a9c4086108cdb26da1b89a40b59b
nilsherzig <nils@nilsherzig.com>
2023-09-24 21:11:40 +0200

    csad

--- add_k3s_to_kubekonfig.sh
+++ add_k3s_to_kubekonfig.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -xe 
 
-export KUBECONFIG=~/.kube/config:/etc/rancher/k3s/k3s.yaml:./k3s.yaml
+export KUBECONFIG=~/.kube/config:"$1"
 kubectl config view --flatten > /tmp/kubeconfig.yaml.tmp
 cp ~/.kube/config ~/.kube/config.bak
 cp /tmp/kubeconfig.yaml.tmp ~/.kube/config

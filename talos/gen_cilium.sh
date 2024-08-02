0a7546d8e22aef196bb2f04eb6d594bfbb33baf3
Nils Herzig <git@nilsherzig.com>
2024-08-02 21:34:31 +0200

    rename and gwapi things

--- talos/gen_cilium.sh
+++ talos/gen_cilium.sh
@@ -4,23 +4,21 @@ helm template \
     cilium \
     cilium/cilium \
     --namespace kube-system \
-    --set bandwidthManager.enabled=false \
     --set bpf.hostLegacyRouting=false \
     --set cgroup.autoMount.enabled=false \
     --set cgroup.hostRoot=/sys/fs/cgroup \
+    --set gatewayAPI.enabled=true \
     --set hubble.relay.enabled=true \
     --set hubble.ui.enabled=true \
-    --set ingressController.default=true \
-    --set ingressController.enabled=true \
-    --set ingressController.enforceHttps=true \
-    --set ingressController.hostNetwork.enabled=true \
-    --set ingressController.hostNetwork.sharedListenerPort="8080" \
-    --set ingressController.loadbalancerMode=shared  \
     --set ipam.mode=kubernetes \
     --set k8sServiceHost=localhost \
     --set k8sServicePort=7445 \
     --set kubeProxyReplacement=true \
     --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
     --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
-    --version 1.16.0-pre.3 > cilium.yaml
+    --version 1.16.0 > cilium.yaml
 
+    # --set loadBalancer.dsrDispatch=geneve \
+    # --set loadBalancer.mode=dsr \
+    # --set routingMode=tunnel \
+    # --set tunnelProtocol=geneve \
